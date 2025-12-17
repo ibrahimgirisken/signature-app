715e92c16878fdb8ee8ee8aa0cfc8a399cc10645
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-12-17 16:56:08 +0300

    2025-12-17-16-56

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>404</h1>
+      <p>Sayfa bulunamadı.</p>
+    </main>
+  )
+}
+
+export default NotFound
