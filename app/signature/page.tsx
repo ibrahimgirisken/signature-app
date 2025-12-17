d99773759e9853039f018131d984e5d9d6ac411d
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-12-17 17:53:48 +0300

    2025-12-17-17-53

--- app/signature/page.tsx
+++ app/signature/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ClientForm from './client-form'
+import SignatureView from './signature-view'
 
 function Signature() {
   return (
     <>
     <ClientForm/>
+    <SignatureView/>
     </>
   )
 }
---
+++ app/signature/signature-view.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function SignatureView() {
+  return (
+    <>
+    <Container className='mb-5'>
+    SignatureView
+    </Container>
+    </>
+  )
+}
+
+export default SignatureView
