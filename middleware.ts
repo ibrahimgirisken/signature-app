01b94dd422a1d86492718fcda9fd7424ef114e58
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2026-02-19 17:13:40 +0300

    2026-02-19-17-13

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,16 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // Cookie'den token'ı çekiyoruz
   const token = request.cookies.get('token')?.value;
 
-  // Eğer token yoksa ve kullanıcı /admin ile başlayan bir yere gitmeye çalışıyorsa
   if (!token && request.nextUrl.pathname.startsWith('/admin')) {
-    const loginUrl = new URL('/login', request.url);
-    return NextResponse.redirect(loginUrl);
+    // URL oluştururken .nextUrl kullanarak daha güvenli yönlendirme yapabilirsin
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin/:path*'], // Tüm admin alt sayfalarını kapsar
+  matcher: '/admin/:path*', 
 };
