84c71ac0e0f582afe020a6d567a29b9282980dc9
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2026-02-19 09:41:31 +0300

    2026-02-19-09-41

--- services/base.service.ts
+++ services/base.service.ts
@@ -1,29 +1,35 @@
 import { http } from "@/lib/http";
 
-export class BaseService<TRequest,TResponse> {
-    constructor(private readonly endpoint:string){}
+export class BaseService<TRequest, TResponse> {
+    constructor(private readonly endpoint: string) {}
 
-    async getAll():Promise<TResponse[]>{
-        const res=await http.get<TResponse[]>(`${this.endpoint}/all`)
+    async getAll(params?: TRequest | any): Promise<TResponse[]> {
+        const res = await http.get<TResponse[]>(`${this.endpoint}/all`, {
+            params: params 
+        });
         return res.data;
     }
 
-    async getById(id:string):Promise<TResponse> {
-        const res=await http.get<TResponse>(`${this.endpoint}/getbyid?Id=${id}`)
+    async getById(id: string): Promise<TResponse> {
+        const res = await http.get<TResponse>(`${this.endpoint}/getbyid`, {
+            params: { Id: id }
+        });
         return res.data;
     }
 
-    async create(data:TRequest|any):Promise<TResponse> {
-        const res=await http.post<TResponse>(`${this.endpoint}/add`,data)
+    async create(data: TRequest | any): Promise<TResponse> {
+        const res = await http.post<TResponse>(`${this.endpoint}/add`, data);
         return res.data;
     }
 
-    async update(data:TRequest):Promise<TResponse> {
-        const res=await http.put<TResponse>(`${this.endpoint}/update`,data)
+    async update(data: TRequest | any): Promise<TResponse> {
+        const res = await http.put<TResponse>(`${this.endpoint}/update`, data);
         return res.data;
     }
 
-    async delete(id:string):Promise<void> {
-        const res=await http.delete(`${this.endpoint}/${id}`)
+    async delete(id: string): Promise<void> {
+        await http.delete(`${this.endpoint}/delete`, {
+            params: { Id: id }
+        });
     }
 }
