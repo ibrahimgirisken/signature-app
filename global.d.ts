082c3fa27702b2809608f51f6c343a74ca921e05
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2026-07-16 13:41:44 +0300

    2026-07-16-13-41

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,4 @@
 declare module "*.css" {
   const content: { [className: string]: string };
-  default content;
+ export default content;
 }
--- services/companyComponent.service.ts
+++
@@ -1,4 +0,0 @@
-import { CompanyComponentRequest, CompanyComponentResponse} from "@/types/company";
-import { BaseService } from "./base.service";
-
-export const companyComponentService=new BaseService<CompanyComponentRequest,CompanyComponentResponse>("/CompanyComponent");
